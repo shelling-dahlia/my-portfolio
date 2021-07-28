f09fc1d6d84b711731b151417845b982f80d2555
hetay566 <heaty566@gmail.com>
2021-07-29 04:15:19 +0700

    Test

--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -12,7 +12,7 @@ const NotFound: React.FunctionComponent = () => {
                     <div className="space-y-2 ">
                         <h1 className="text-white text-9xl">404</h1>
                         <h3 className="text-7xl text-mercury-700 text-coolGray-300">Whoops!</h3>
-                        <p className="text-2xl text-mercury-700 text-coolGray-300">The page you were looking for doesn’t existt</p>
+                        <p className="text-2xl text-mercury-700 text-coolGray-300">The page you were looking for doesn’t exist</p>
                     </div>
                 </div>
             </div>
